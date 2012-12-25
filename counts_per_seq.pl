05a761b5a076c136baed95c2bcdad3ba26c47b1c
Mike Covington <mfcovington@gmail.com>
2012-12-24 18:13:57 -0800

    add counts_per_seq.pl (functional)

---
+++ counts_per_seq.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/env perl
+# FILE_NAME.pl
+# Mike Covington
+# created: 2012-12-21
+#
+# Description:
+#
+use strict;
+use warnings;
+use autodie;
+use feature 'say';
+use Data::Printer;
+use Getopt::Long;
+
+my $bam_dir = "./";
+my $out_dir = "./";
+my $prefix  = "";
+my $suffix  = ".bam";
+
+my $options = GetOptions(
+    "bam_dir=s" => \$bam_dir,
+    "out_dir=s" => \$out_dir,
+    "prefix=s"  => \$prefix,
+    "suffix=s"  => \$suffix,
+);
+
+my @bam_files = glob "$bam_dir/$prefix*$suffix";
+
+for (@bam_files) {
+    say "  Processing: $_";
+    my ($bam) = $_ =~ m|.*\/([^\/]*)\.bam|;
+    my %gene_counts;
+    open my $bam_fh, '-|', "samtools view $_";
+    for my $line (<$bam_fh>) {
+        my @elements = split /\s/, $line;
+        $gene_counts{ $elements[2] }++;
+    }
+    open my $out_alpha_fh, '>', "$bam.counts_a";
+    open my $out_count_fh, '>', "$bam.counts_1";
+    say $out_alpha_fh "$_\t$gene_counts{$_}" for sort keys %gene_counts;
+    say $out_count_fh "$_\t$gene_counts{$_}"
+      for sort { $gene_counts{$b} <=> $gene_counts{$a} } keys %gene_counts;
+}
