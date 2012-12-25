bbef8d249193dd970b95c845f38fbebaa0d27061
Mike Covington <mfcovington@gmail.com>
2012-12-25 10:23:51 -0800

    add option to sort by alpha only or num only

--- counts_per_seq.pl
+++ counts_per_seq.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/env perl
-# FILE_NAME.pl
+# counts_per_seq.pl
 # Mike Covington
 # created: 2012-12-21
 #
@@ -16,15 +16,21 @@ my $bam_dir = "./";
 my $out_dir = "./";
 my $prefix  = "";
 my $suffix  = ".bam";
-my $verbose;
+my ( $alpha_only, $num_only, $verbose );
 
 my $options = GetOptions(
-    "bam_dir=s" => \$bam_dir,
-    "out_dir=s" => \$out_dir,
-    "prefix=s"  => \$prefix,
-    "suffix=s"  => \$suffix,
-    "verbose"   => \$verbose,
+    "bam_dir=s"  => \$bam_dir,
+    "out_dir=s"  => \$out_dir,
+    "prefix=s"   => \$prefix,
+    "suffix=s"   => \$suffix,
+    "alpha_only" => \$alpha_only,
+    "num_only"   => \$num_only,
+    "verbose"    => \$verbose,
 );
+
+die
+"  ERROR: Can't sort 'alphabetically only' AND 'numerically only' at the same time.\n"
+  if $alpha_only && $num_only;
 
 my @bam_files = glob "$bam_dir/$prefix*$suffix";
 
@@ -37,9 +43,13 @@ for (@bam_files) {
         my @elements = split /\s/, $line;
         $gene_counts{ $elements[2] }++;
     }
-    open my $out_alpha_fh, '>', "$bam.counts_a";
-    open my $out_count_fh, '>', "$bam.counts_1";
-    say $out_alpha_fh "$_\t$gene_counts{$_}" for sort keys %gene_counts;
-    say $out_count_fh "$_\t$gene_counts{$_}"
-      for sort { $gene_counts{$b} <=> $gene_counts{$a} } keys %gene_counts;
+    unless ($num_only) {
+        open my $out_alpha_fh, '>', "$bam.counts_a";
+        say $out_alpha_fh "$_\t$gene_counts{$_}" for sort keys %gene_counts;
+    }
+    unless ($alpha_only) {
+        open my $out_count_fh, '>', "$bam.counts_1";
+        say $out_count_fh "$_\t$gene_counts{$_}"
+          for sort { $gene_counts{$b} <=> $gene_counts{$a} } keys %gene_counts;
+    }
 }
