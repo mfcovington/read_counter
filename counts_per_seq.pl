0768953dd017581d515ae3f6fc2891f84a629d28
Mike Covington <mfcovington@gmail.com>
2012-12-25 11:01:47 -0800

    add usage/help statement

--- counts_per_seq.pl
+++ counts_per_seq.pl
@@ -12,11 +12,30 @@ use feature 'say';
 use Data::Printer;
 use Getopt::Long;
 
+my $usage = <<EOF;
+
+    USAGE:
+    $0
+        --bam_dir       Directory containing .bam files [./]
+        --out_dir       Output directory [./]
+        --prefix        Filename prefix †
+        --suffix        Filename suffix † [.bam]
+        --alpha_only    Sort by sequence name ‡
+        --num_only      Sort by number of counts ‡
+        --verbose
+        --help
+
+        † A custom prefix/suffix combo can be used to restrict input files
+        ‡ If no sorting options are chosen, two files are output for each
+          input (one sorted by sequence name and the other by # of counts)
+
+EOF
+
 my $bam_dir = "./";
 my $out_dir = "./";
 my $prefix  = "";
 my $suffix  = ".bam";
-my ( $alpha_only, $num_only, $verbose );
+my ( $alpha_only, $num_only, $verbose, $help );
 
 my $options = GetOptions(
     "bam_dir=s"  => \$bam_dir,
@@ -26,8 +45,10 @@ my $options = GetOptions(
     "alpha_only" => \$alpha_only,
     "num_only"   => \$num_only,
     "verbose"    => \$verbose,
+    "help"       => \$help,
 );
 
+die $usage if $help;
 die
 "  ERROR: Can't sort 'alphabetically only' AND 'numerically only' at the same time.\n"
   if $alpha_only && $num_only;
