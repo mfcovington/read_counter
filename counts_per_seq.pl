8bfbd174bc750b757431ae7ba3ba0f49d56caa6d
Mike Covington <mfcovington@gmail.com>
2012-12-26 00:21:29 -0800

    add option to count reads for a subset of sequences

--- counts_per_seq.pl
+++ counts_per_seq.pl
@@ -9,7 +9,6 @@ use strict;
 use warnings;
 use autodie;
 use feature 'say';
-use Data::Printer;
 use Getopt::Long;
 
 my $usage = <<EOF;
@@ -18,6 +17,7 @@ my $usage = <<EOF;
     $0
         --bam_dir        Directory containing .bam files [./]
         --out_dir        Output directory [./]
+        --seq_list       File containing sequences to count (Default: all sequences)
         --consolidate    Delimiter between Sample ID and Rep ID *
         --prefix         Filename prefix †
         --suffix         Filename suffix † [.bam]
@@ -39,12 +39,13 @@ my $bam_dir = "./";
 my $out_dir = "./";
 my $prefix  = "";
 my $suffix  = ".bam";
-my ( $consolidate, $alpha_only, $num_only, $verbose, $help );
+my ( $consolidate, $seq_file, $alpha_only, $num_only, $verbose, $help );
 
 my $options = GetOptions(
     "bam_dir=s"     => \$bam_dir,
     "out_dir=s"     => \$out_dir,
     "consolidate=s" => \$consolidate,
+    "seq_file=s"    => \$seq_file,
     "prefix=s"      => \$prefix,
     "suffix=s"      => \$suffix,
     "alpha_only"    => \$alpha_only,
@@ -69,6 +70,15 @@ if ( defined $consolidate ) {
     @bam_file_list = sort keys %bam_groups;
 }
 
+my %seq_list;
+if ( defined $seq_file ) {
+    open my $seq_fh, '<', $seq_file;
+    chomp( my @seqs = <$seq_fh> );
+    $seq_list{$_}++ for @seqs;
+    my $seq_count = scalar keys %seq_list;
+    say "  Found $seq_count sequences in $seq_file" if $verbose;
+}
+
 for my $bam (@bam_file_list) {
     say "  Combining:  $bam" if $verbose && defined $consolidate;
     my %gene_counts;
@@ -77,10 +87,12 @@ for my $bam (@bam_file_list) {
         open my $bam_fh, '-|', "samtools view $_";
         for my $line (<$bam_fh>) {
             my @elements = split /\s/, $line;
+            next unless defined $seq_file && exists $seq_list{ $elements[2] };
             $gene_counts{ $elements[2] }++;
         }
     }
     my ($id) = $bam =~ m|.*\/([^\/]*)(?(?{ !defined $consolidate; })\.bam)|;
+    $id .= ".$seq_file" if defined $seq_file;
     unless ($num_only) {
         open my $out_alpha_fh, '>', "$out_dir/$id.counts_a";
         say $out_alpha_fh "$_\t$gene_counts{$_}" for sort keys %gene_counts;
