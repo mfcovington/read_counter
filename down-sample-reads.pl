2add57640e02a7975676e81b1374467ec51d2506
Mike Covington <mfcovington@gmail.com>
2015-01-29 22:58:24 -0800

    Get output directory and down-sample fraction via CLI

--- down-sample-reads.pl
+++ down-sample-reads.pl
@@ -8,13 +8,23 @@ use strict;
 use warnings;
 use autodie;
 use feature 'say';
+use File::Path 'make_path';
+use Getopt::Long;
+
+my $fraction;
+my $out_dir = '.';
+
+my $options = GetOptions(
+    "out_dir=s"  => \$out_dir,
+    "fraction=f" => \$fraction,
+);
 
 my @alignments_file_list = @ARGV;
 
 my $alignment_file = $alignments_file_list[0];
-my $output_file    = "downsampled.sam";
-my $fraction       = 0.2;
+my $output_file    = "$out_dir/downsampled.sam";
 
+make_path $out_dir;
 downsample_reads( $alignment_file, $output_file, $fraction );
 
 exit;
