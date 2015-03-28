b857e3a78e4af0e1bc926355550bc7baa742eb19
Mike Covington <mfcovington@gmail.com>
2015-03-28 00:36:33 -0700

    Fix typo in man page for 'down-sample-reads-auto.pl'

--- bin/down-sample-reads-auto.pl
+++ bin/down-sample-reads-auto.pl
@@ -20,11 +20,11 @@ use read_counter;
 
 =head1 TITLE
 
-down-sample-reads.auto.pl - Down-sample reads in bulk w/ auto-calc'd fractions
+down-sample-reads-auto.pl - Down-sample reads in bulk w/ auto-calc'd fractions
 
 =head1 SYNOPSIS
 
-perl down-sample-reads.auto.pl [options] <Alignment file(s)>
+perl down-sample-reads-auto.pl [options] <Alignment file(s)>
 
 =head1 DESCRIPTION
 
